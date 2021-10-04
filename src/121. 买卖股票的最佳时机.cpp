8497b767483309c2dae6deb1a0b3f7203dbf675e
Levi Zhang <levizhangcs@outlook.com>
2021-10-04 23:34:20 +0800

    Create 121. 买卖股票的最佳时机.cpp

---
+++ src/121. 买卖股票的最佳时机.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int maxProfit(vector<int>& prices) {
+        int minprice = 1e9;
+        int maxprofit = 0;
+        for (int price : prices) {
+            maxprofit = max(maxprofit, price - minprice);
+            minprice = min(minprice, price);
+        }
+
+        return maxprofit;
+    }
+};
